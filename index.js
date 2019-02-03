f0cbc6d85e358efed332f9556484eb13018119fe
Leandro Gaspar Scanavini <leandrogaspar789@gmail.com>
2019-02-03 12:13:34 -0200

    Added missing dependencies

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-exports.test = [
-    'hello',
-    'world'
-];
+const ws = require('./lib/flows/ws.json');
+const rest = require('./lib/flows/rest.json');
+const selenium = require('./lib/flows/selenium.json');
+
+exports.ws = ws;
+exports.rest = rest;
+exports.selenium = selenium;
